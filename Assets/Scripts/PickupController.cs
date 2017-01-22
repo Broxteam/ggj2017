c16b610745fd6b222a4726600ce18c78234481f4
sg13 <sgiraud@e-cml.org>
2017-01-22 04:29:42 +0100

    Frigo

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -6,7 +6,7 @@ public class PickupController : MonoBehaviour {
 
 	public float gravityScale;
 	private GameObject fridgectrl;
-	private CircleCollider2D cc2d;
+	private BoxCollider2D cc2d;
 	private Rigidbody2D rb2d;
 	private float fallspeed;
 	private float rndmlateral;
@@ -14,8 +14,8 @@ public class PickupController : MonoBehaviour {
 
 	void Start() {
 		rb2d = GetComponent<Rigidbody2D> ();
-		cc2d = GetComponent<CircleCollider2D> ();
-		fallspeed = 0.14f;
+		cc2d = GetComponent<BoxCollider2D> ();
+		fallspeed = 0.1f;
 		falling = true;
 		rndmlateral = (Random.value * 2.0f - 1.5f);
 
@@ -26,16 +26,23 @@ public class PickupController : MonoBehaviour {
 			fallspeed -= gravityScale * Time.deltaTime;
 			transform.position += new Vector3 (rndmlateral / (Random.Range(8f, 25f)), 1.0f * fallspeed, 0.0f) ;
 		}
+
+		if (transform.position.y < -1.1f) {
+			Destroy (this.gameObject);
+		}
+
+
 	}
 
 	void FixedUpdate() {
-		RaycastHit2D hit = Physics2D.Raycast(transform.position - Vector3.up * cc2d.radius, -Vector2.up, 0.01f);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position - (Vector3.up * (cc2d.size.y)/2), -Vector2.up, 0.001f);
 		if (hit.collider != null) {
 			if (hit.collider.gameObject.CompareTag ("Ground")) {
 				fallspeed = 0.0f;
 				falling = false;
 			}
 		}
+
 	}
 
 	void SetFridge(GameObject fctrl) {
